1c16a33c8d7339b289722dd9e3d858234d66b4da
Isyana <lattermievaa@gmail.com>
2026-04-09 11:28:47 +0700

    change the koneksi service

--- service/database.php
+++ service/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database_name = "kas_kelas";
+$hostname = "sql211.infinityfree.com";
+$username = "if0_41500233";
+$password = "HpQv0jy5gz";
+$database_name = "if0_41500233_kas_kelas";
 
 $db = mysqli_connect($hostname, $username, $password, $database_name);
 
